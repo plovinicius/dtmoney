e1279acb06287ac7bf6e7aa6ad6bd13b0806a8bf
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-04-12 13:34:20 -0300

    feat(modal/forms): Configurando modal com react-modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="dt money"/>
+        <img src={logoImg} alt="dt money" />
 
-        <button type="button">
+        <button onClick={onOpenNewTransactionModal} type="button">
           Nova transacao
         </button>
       </Content>
