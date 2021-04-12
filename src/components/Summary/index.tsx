d895b41d49292f0293f9b5b74dba717659bdacea
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-04-12 17:02:28 -0300

    feat(contextos/hooks): Calculando resumo de transacoes

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,6 +8,23 @@ import { Container } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionContext);
 
+  // acc = acumulator
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  });
+
   return (
     <Container>
       <div>
@@ -16,7 +33,12 @@ export function Summary() {
           <img src={incomeImg} alt="Entradas"/>
         </header>
 
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
@@ -25,7 +47,13 @@ export function Summary() {
           <img src={outcomeImg} alt="Saidas"/>
         </header>
 
-        <strong>- R$500,00</strong>
+        <strong>
+          -
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
       </div>
 
       <div className="highlight-background">
@@ -34,7 +62,12 @@ export function Summary() {
           <img src={totalImg} alt="Total"/>
         </header>
 
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
