3512c2261a78578b78624b116ee42612ee0d6cf5
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-04-12 14:09:12 -0300

    feat(forms/modals): Criando botoes do tipo e estilizando com a lib polished

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean,
@@ -24,6 +26,18 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <input placeholder="Titulo"/>
 
         <input type="number" placeholder="Valor"/>
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saida"/>
+            <span>Saida</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input placeholder="Categoria"/>
 
